e00484e7a928d4baae4804fefbed10ae8a352fc2
dat.ht <huynhtandat223@gmail.com>
2025-03-01 12:26:38 +0700

    t

--- apps/CFW.AppHost/Program.cs
+++ apps/CFW.AppHost/Program.cs
@@ -1,13 +1,12 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
 
-//var api = builder.AddProject<Projects.CFW_EntityApi_TestApi>("test-api")
-//    .WithEnvironment("DbContextSetting__SqliteConnectionString", "Data Source=test-api.db")
-//    .WithExternalHttpEndpoints();
+var api = builder.AddProject<Projects.CFW_EntityApi_TestApi>("test-api")
+    .WithEnvironment("DbContextSetting__SqliteConnectionString", "Data Source=test-api.db")
+    .WithExternalHttpEndpoints();
 
 var auto = builder.AddProject<Projects.CFW_Automation>("cfw-automation")
     .WithEnvironment("DatabaseSetting__SqliteConnectionString", "Data Source=auto-api.db");
-
 
 builder.AddNpmApp("test-ui", "../CFW.EntityApi.TestUI")
     .WithReference(auto)
